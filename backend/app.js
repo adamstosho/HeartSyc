0344fe7c9c52ef04d73b2d8600326868b18414e7
adamstosho <omoridoh111@gmail.com>
2025-07-19 00:39:22 +0100

    moderation of .env file

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,11 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://heartsync-0boj.onrender.com",
+  credentials: true
+}));
+
 app.use(helmet());
 app.use(morgan('dev'));
 
